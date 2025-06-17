3aa0f3e66847b1b90f419bc03d2316e33ef0706c
almasiks <magrupovalmas06@gmail.com>
2025-06-17 15:17:09 +0500

    Create lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,111 @@
+/*#include <iostream>                // task1
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+string a;
+cin>>a;
+cout<<"Hello, "<<a;
+    return 0;
+}
+
+#include <iostream>                // task2
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int a, b, sum;
+cin>>a>>b;
+sum = a+b;
+cout<<sum;
+    return 0;
+}
+   
+   #include <iostream>                // task3
+#include <bits/stdc++.h>
+using namespace std; 
+int main() {
+double a, s;
+cin>>a;
+s = a*a* 3.14;
+cout<<s;
+
+
+    return 0;
+} 
+#include <iostream>                // task4
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int a, age;
+cin>>a;
+age = a *12;
+cout<<"You are "<< age<< " months old.";
+    return 0;
+}
+#include <iostream>                // task5
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+double a, res;
+cin>>a;
+res = (a * 9/5) + 32.0;
+cout<<res;
+    return 0;
+}
+#include <iostream>                // task6
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int a;
+cin>>a;
+cout<<"The square is "<< a*a;
+    return 0;
+}
+#include <iostream>                // task7
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int p,r,t, res;
+cin>>p>>r>>t;
+res = (p*r*t)/100;
+cout<<"Simple interest is "<<res;
+    return 0;
+}
+#include <iostream>                // task8
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+double w, h, res, h1;
+cin>>w>>h;
+h1 = h*h;
+res = w/h1;
+cout<<"Your BMI is "<<res;
+
+    return 0;
+}
+#include <iostream>                // task9
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+double a, b, c, ave;
+cin>>a>>b>>c;
+ave = (a+b+c)/3;
+cout<<"The average is "<<ave;
+
+    return 0;
+}
+   
+#include <iostream>                // task10
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int a;
+cin>>a;
+if(a%2==0){
+    cout<<"The number is even";
+}
+else{
+    cout<<"The number is odd";
+}
+    return 0;
+} 
+*/
